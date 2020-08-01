5527d2a79f4af31155ffbf0bd737c06477d10b77
Fran Jurmanović <fjurma12@outlook.com>
2020-08-01 11:01:23 +0200

    Layout works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-dashboard';
+  sidebarOn = true;
+
+  toggleMenu() {
+    this.sidebarOn = !this.sidebarOn;
+  }
 }
