dd74a1d2542ea338c78e428691f052264fe64afa
Fran Jurmanović <fjurma12@outlook.com>
2020-09-24 15:31:46 +0200

    Styling: project select on header

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +9,21 @@ import {AuthService} from '../auth.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public router: Router) { }
 
   sidebarOn = true;
+  projectChange(e) {
+    if (e.target.value === 'new') {
+      this.authService.selectProject(null);
+      this.router.navigate(['/dashboard/new']);
+    } else if (e.target.value === 'select') {
+      this.authService.selectProject(null);
+      this.router.navigate(['dashboard']);
+    } else {
+      this.authService.selectProject(e.target.value);
+      //this.router.navigate(['/dashboard/info'], {queryParams: {project: this.authServices.selectedId}});
+    }
+  }
 
   toggleMenu() {
     const sidebar = document.querySelector('.sidebar');
